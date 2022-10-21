60fa5a29f2e40a3505bc4490d4996834f57e17ad
Juliana Resplande <julianarsg13@gmail.com>
2022-10-20 21:44:00 -0300

    actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,69 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action, Tracker
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+from constants import PIZZAS, DRINKS, PRICE_TABLES
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+class ActionListPizza(Action):
+    def name(self) -> Text:
+        return "action_list_pizza"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        message = "Temos disponíveis as pizzas de " + " ,".join(PIZZAS)
+        dispatcher.utter_message(text=message)
+
+        return []
+
+
+class ActionListDrinks(Action):
+    def name(self) -> Text:
+        return "action_list_pizza"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        message = "Temos disponíveis as seguintes bebidas: " + " ,".join(DRINKS)
+        dispatcher.utter_message(text=message)
+
+        return []
+
+
+class ActionShowTotal(Action):
+    def name(self) -> Text:
+        return "action_show_total"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        message = ""
+
+        total = 0
+        for key in ("pizza", "drink"):
+            item = tracker.get(key)
+            value = PRICE_TABLES.get(item, 15)
+
+            message += f"{item} = R$ {value},00\n"
+            total += value
+
+        message += f"Total = R$ {total},00"
+
+        dispatcher.utter_message(text=message)
+
+        return []
---
+++ actions/constants.py
@@ -0,0 +1,40 @@
+PIZZAS = [
+    "quatro-queijos",
+    "marguerita",
+    "lombo",
+    "bacon",
+    "portuguesa",
+    "peperoni",
+    "muçarela",
+    "frango com catupiry",
+    "brigadeiro",
+    "banana com canela",
+]
+
+DRINKS = [
+    "coca-cola",
+    "guaraná",
+    "guaraná jesus",
+    "pepsi",
+    "suco de laranja" "suco de limão",
+]
+
+
+PRICE_TABLES = {
+    "quatro-queijos": 30,
+    "marguerita": 25,
+    "lombo": 23,
+    "bacon": 20,
+    "portuguesa": 25,
+    "peperoni": 20,
+    "muçarela": 20,
+    "frango com catupiry": 23,
+    "brigadeiro": 25,
+    "banana com canela": 23,
+    "coca-cola": 6,
+    "guaraná": 6,
+    "guaraná jesus": 6,
+    "pepsi": 6,
+    "suco de laranja": 7,
+    "suco de limão": 5,
+}
